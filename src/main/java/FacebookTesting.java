62a8f6c05922ce5f15df37e1b9bab2bd424a63f9
harashchuk.vadym <harashchuk.vadym@playtika.com>
2019-05-22 19:54:07 +0300

    modified:   src/main/java/FacebookTesting.java

--- src/main/java/FacebookTesting.java
+++ src/main/java/FacebookTesting.java
@@ -1,6 +1,8 @@
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.springframework.social.facebook.api.*;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
 
 import java.io.IOException;
 
@@ -9,18 +11,31 @@ public class FacebookTesting {
     private final static String URL = "https://graph.facebook.com/";
     private final static String USER_ID = "105607550683592";
     private OkHttpClient httpClient = new OkHttpClient();
-    private static String userAT = "EAAFvhvPUG3gBAB1nuZAcZAae62H0gCsw0W6FgkJOastTYfoxhNCxYB8fdxr6NWhV8PUkCpbIeNaQ" +
-            "cjxFow6S3IgKgYC0E6D4e6MJOZAMK4z6lt1KZCEX0tNCzmNfrAXeBS20HE3VKBzAVqUNDfOLXx8qDjz38FmBNZBb2aJR" +
-            "ksir3ELPH0fQWeXeXP1WpMcdmZBo2Vzd6UigZDZD";
+
+        //"https://graph.facebook.com/{your-page-post-id}
+        //        ?message=Happy%20Holidays!
+        //    &access_token={your-page-access-token}"
+
+    private static String userAT = "EAAFvhvPUG3gBACGxZCszbZCEIRGGxVTPoex2E" +
+            "aZAH3KSAHEMAKYZAsaYLLfEWHfEkUOfQnTjxHRiCEhRSw2njnqiTyZBZCeBn2laDZBUDmvL" +
+            "ib7jOY5A8luiABELCzS5UZCB1CIO5VIGfZCva8jUx1qUToGSR" +
+            "Om2lo19EBF4NWM4vROfnYst7KE0HWGbL7Kf9WH6vLoLcRyDQSAZDZD";
 
     public static void main(String[] args) throws IOException {
 
+        Facebook facebook = new FacebookTemplate(userAT);
+        facebook.feedOperations().post(new PostData(USER_ID).message("Hello"));
+        PagedList<Post> posts = facebook.feedOperations().getPosts();
+        for (Post p : posts) {
+            System.out.println(p.getMessage());
+        }
+
         FacebookTesting facebookApi = new FacebookTesting();
-        String response = facebookApi.readPosts(USER_ID + "/feed/?access_token=" + userAT);
-        System.out.println(response);
+
     }
 
-    private String readPosts(String endPoint) throws IOException {
+
+    private String post(String endPoint) throws IOException {
         Request request = new Request.Builder()
                 .url(URL + endPoint)
                 .build();
